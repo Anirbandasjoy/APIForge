f633393ba084232f3f518e53a732baafbac85953
Anirbandasjoy <joy600508@gmail.com>
2025-06-23 03:13:26 +0600

    env

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -2,6 +2,7 @@ export const USER_ROLES = {
   ADMIN: 'admin',
   MODERATOR: 'moderator',
   SUPERADMIN: 'superadmin',
+  STUDENT: 'student',
 } as const;
 
 export type UserRole = (typeof USER_ROLES)[keyof typeof USER_ROLES];
